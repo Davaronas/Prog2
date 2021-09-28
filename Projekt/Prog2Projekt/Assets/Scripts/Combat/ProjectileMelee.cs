d9984ab7c65e5562e4a8e9eefcf3f9ef3524d6a7
Dávid Bankus <dbankus52@gmail.com>
2021-09-28 22:42:52 +0200

    working on the first map

--- Projekt/Prog2Projekt/Assets/Scripts/Combat/ProjectileMelee.cs
+++ Projekt/Prog2Projekt/Assets/Scripts/Combat/ProjectileMelee.cs
@@ -12,6 +12,9 @@ public class ProjectileMelee : Projectile
     {
         ps = transform.root.GetComponentInChildren<ParticleSystem>();
         ps.transform.parent = null;
+
+        Destroy(gameObject, ps.main.duration + ps.main.startLifetime.constantMax); // amint az összes particle elpusztult akkor pusztítsuk el a gameobjectet, ezután ne tudjon sebezni
+        Destroy(ps.gameObject, 2f); // a particle rendszert is pusztítsuk el, ez igazából mindegy mikor csak a performance miatt jó
     }
 
     void Start()
@@ -22,5 +25,7 @@ public class ProjectileMelee : Projectile
         
     }
 
+
+   
    
 }
